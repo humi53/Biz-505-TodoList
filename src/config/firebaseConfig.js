8126053ed82591323a4ee1f71657f593173819f1
humi53 <j.k.humi9@gmail.com>
2023-08-28 13:39:13 +0900

    clear

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5q7RJL9NDA_SR6DHaRyC-BCTHjkXSJ00",
+  authDomain: "react-humi53.firebaseapp.com",
+  projectId: "react-humi53",
+  storageBucket: "react-humi53.appspot.com",
+  messagingSenderId: "732791248737",
+  appId: "1:732791248737:web:94c9d6643e3090a7d31720",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
